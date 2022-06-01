0a565190ffaf21cccc7af2c60498fd06272d53d7
LuciaMejia1996 <luciamejia262@hotmail.com>
2022-06-01 17:25:04 +0200

    new script of example coursera

---
+++ Examples_Cursera/correction_example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+
+#Este es parte de un ejercicio que se realizo en una prueba 
+sudo mv /var/www/example/about_us.html /var/www/example/aboutus.html
+sudo chmod 644 /var/www/example/aboutus.html
+
